d4597c4d896093d000dd11695cd547d5454ec8d8
Simone Magnani <simonemagnani.96@gmail.com>
2019-08-29 17:14:37 +0200

    Modified executable name and added define to main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "Profiler.h"
 
+#define N_ITER 100
+
 int main(int argc, char **argv) {
 
   //Benchmarking
-  for(int i=0; i<100; i++) {
+  for(int i=0; i<N_ITER; i++) {
     CHECKPOINT("A");
   }
-  ENDPOINT("C");
+  ENDPOINT("A");
 
   return 0;
 }
